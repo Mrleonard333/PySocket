741e35332e076c1c26a25cd3d0319819e23b674f
Mrleonard <listas036@gmail.com>
2022-10-16 16:52:08 -0300

    V_1.0

--- Main.py
+++ Main.py
@@ -5,15 +5,18 @@ from os import system
 connected = set()
 
 async def server(websocket):
-    connected.add(websocket)
+    connected.add(websocket) # < Store the WebSocket connection
 
+                # v Store the messages of the WebSocket connection
     async for message in websocket:
 
+                # v If the message is equal to the Terminate message
             if message == "<^798754613465461987513231871235952721487421^>":
-                connected.remove(websocket)
+                connected.remove(websocket) # < Remove the connection
 
+              # v Store the connections of the connected set
             for C in connected:
-                
+                    # v If the connection is diferent from the WebSocket
                 if C != websocket:
                     if message == "<^798754613465461987513231871235952721487421^>":
                         await C.send(f"[User has been disconected]")
@@ -29,8 +32,9 @@ async def server(websocket):
 async def main():
     system('cls')
     print('[Starting the WebScoket connection]')
+                                # v Handler         # v Port
+    async with websockets.serve(server, "localhost", 5000): # < Start the connection
+        await asyncio.Future()              # ^ Host
+                # ^ Make a loop
 
-    async with websockets.serve(server, "localhost", 5000):
-        await asyncio.Future()
-
-asyncio.run(main())
+asyncio.run(main()) # < Start the main system
